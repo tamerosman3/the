6eacda205e5e976b90aa5c3b7e580930cb72a1a4
tamerosman3 <tamerosman3@gmail.com>
2023-05-02 16:01:46 +0200

    added status and name

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,22 @@ fn main() {
         let pid = process.expect("Failed to get process").pid();
         let mut proc = Process::new(pid).unwrap();
         let cpu_time = proc.cpu_times().unwrap();
-        let total_time = cpu_time.user().as_nanos() + cpu_time.system().as_nanos() + 
+        let total_time = cpu_time.user().as_nanos() + cpu_time.system().as_nanos() +
             cpu_time.children_user().as_nanos() + cpu_time.children_system().as_nanos();
-        let mem_con = proc.memory_percent().unwrap() * 100.0;
+        let mem_con = proc.memory_percent().unwrap();
         let cpu_con = proc.cpu_percent().unwrap();
+        let process_name = proc.name().unwrap();
+        let status = proc.status().unwrap();
 
         // Add the data for the current process to the rows vector
-        rows.push((pid, total_time, mem_con, cpu_con));
+        rows.push((pid, total_time, mem_con, cpu_con, process_name, status));
     }
 
     // Print the table header
-    println!("{:<10} {:<10} {:<10} {:<10}", "PID", "CPU Time", "Memory Consumption", "CPU Consumption");
+    println!("{:<10} {:<15} {:<15} {:<10} {:<15} {:<10}", "PID", "CPU Time", "Memory%", "CPU%", "Process Name", "Status");
 
     // Print the table rows
     for row in rows {
-        println!("{:<10} {:<10} {:<10} {:<10}", row.0, row.1, row.2, row.3);
+        println!("{:<10} {:<15} {:<15.2} {:<10.2} {:<15} {:<10}", row.0, row.1, row.2, row.3, row.4, format!("{:?}", row.5));
     }
 }
