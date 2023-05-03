0639665dabeabd9c045a1a7b437ce5aa6be91ed2
tamerosman3 <tamerosman3@gmail.com>
2023-05-03 07:53:17 +0200

    fixing cpu values again

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,11 @@ fn get_process_data(system: &mut sysinfo::System) -> Vec<(i32, f32, f64, String,
 
     system.refresh_all();
 
-    //let num_processors = system.get_processors().len() as f32;
+    let num_processors = system.get_processors().len() as f32;
 
 
     for (pid, process) in system.get_processes() {
-        let cpu_percent = process.cpu_usage();
+        let cpu_percent = process.cpu_usage() / num_processors;
         let mem_percent =
             (process.memory() as f64 / system.get_total_memory() as f64) * 100.0;
         let process_name = process.name().to_string();
